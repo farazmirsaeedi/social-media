81898412b725cd2ab24cb6b819efca2d9b9d175b
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 06:31:22 +1200

    set dark mode setting

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -55,7 +55,7 @@ const Add = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box width={400} height={300} bgcolor="white" p={3} borderRadius={5}>
+        <Box width={400} height={300} p={3} borderRadius={5} bgcolor={"background.default"} color={"text.primary"}>
           <Typography variant="h6" color="gray" textAlign="center">
             Create Post
           </Typography>
