a9ddd624264b6560e5c87d5f45895c9385e8cb63
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 06:31:06 +1200

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Add from "./Components/Add";
 import Rightbar from "./Components/Rightbar";
 
 function App() {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState("light");
   const darkTheme = createTheme({
     palette: {
       mode: mode,
@@ -16,9 +16,9 @@ function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
         <Navbar />
-        <Stack direction="row" spacing={2} justifyContent="space-evenly">
+        <Stack direction="row" spacing={2} justifyContent="space-evenly" bgcolor={"background.default"} color={"text.primary"}>
           <Sidebar mode={mode} setMode={setMode} />
           <Feed />
           <Rightbar />
