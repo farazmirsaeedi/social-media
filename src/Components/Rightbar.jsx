f44008f1cfff744c6dffe7384b5e902361c84223
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 04:43:27 +1200

    add Latest Images section under avatar

--- src/Components/Rightbar.jsx
+++ src/Components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, AvatarGroup, Typography, ListItem, ListItemAvatar, ListItemText, Divider } from "@mui/material";
+import { Avatar, Box, AvatarGroup, Typography, ImageList, ImageListItem } from "@mui/material";
 import React from "react";
 
 const Rightbar = () =>{
@@ -14,71 +14,25 @@ const Rightbar = () =>{
                     <Avatar alt="Agnes Walker" src="http://material-ui.com/static/images/avatar/4.jpg" />
                     <Avatar alt="Trevor Henderson" src="http://material-ui.com/static/images/avatar/5.jpg" />
                 </AvatarGroup>
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                        <Avatar alt="Remy Sharp" src="http://material-ui.com/static/images/avatar/1.jpg" />
-                        </ListItemAvatar>
-                        <ListItemText
-                        primary="Brunch this weekend?"
-                        secondary={
-                            <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline' }}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                Ali Connors
-                            </Typography>
-                            {" — I'll be in your neighborhood doing errands this…"}
-                            </React.Fragment>
-                        }
-                        />
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                        <Avatar alt="Travis Howard" src="http://material-ui.com/static/images/avatar/2.jpg" />
-                        </ListItemAvatar>
-                        <ListItemText
-                        primary="Summer BBQ"
-                        secondary={
-                            <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline' }}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                to Scott, Alex, Jennifer
-                            </Typography>
-                            {" — Wish I could come, but I'm out of town this…"}
-                            </React.Fragment>
-                        }
-                        />
-                    </ListItem>
-                    <Divider variant="inset" component="li" />
-                    <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar alt="Cindy Baker" src="http://material-ui.com/static/images/avatar/4.jpg" />
-                        </ListItemAvatar>
-                        <ListItemText
-                        primary="Oui Oui"
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    Sandra Adams
-                                </Typography>
-                            {' — Do you have Paris recommendations? Have you ever…'}
-                            </React.Fragment>
-                        }
-                        />
-                    </ListItem>
+                <Typography variant="h6" fontWeight={100}>
+                    Latest Images
+                </Typography>
+                <ImageList cols={3} rowHeight={100} gap={5}>
+                    <ImageListItem>
+                        <img src="https://images.unsplash.com/photo-1588436706487-9d55d73a39e3"/>
+                    </ImageListItem>
+                    
+                    <ImageListItem>
+                        <img src="https://images.unsplash.com/photo-1563298723-dcfebaa392e3"/>
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://images.unsplash.com/photo-1588436706487-9d55d73a39e3"/>
+                    </ImageListItem>
+                </ImageList>
+
+         
+
+
             </Box>
         </Box>
         
