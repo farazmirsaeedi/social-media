cea40a19c47f91b2e9699c356e89047249ba966c
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 05:57:55 +1200

    add darkTheme in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import { Box, Stack } from '@mui/material';
-import Sidebar from './Components/Sidebar';
-import Feed from './Components/Feed';
-import Add from './Components/Add';
-import Rightbar from './Components/Rightbar';
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import { Box, Stack, createTheme, ThemeProvider } from "@mui/material";
+import Sidebar from "./Components/Sidebar";
+import Feed from "./Components/Feed";
+import Add from "./Components/Add";
+import Rightbar from "./Components/Rightbar";
 
 function App() {
-
   const [mode, setMode] = useState("dark");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
 
   return (
-    <Box>
-    <Navbar/>
-    <Stack direction="row" spacing={2} justifyContent="space-evenly">
-      <Sidebar/>
-      <Feed/>
-      <Rightbar/>
-    </Stack>
-    <Add/>
-  </Box>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-evenly">
+          <Sidebar mode={mode} setMode={setMode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
