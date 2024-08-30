e315c7d8c183c8a289d26ab5ec491b5e37af8c85
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 05:46:48 +1200

    refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
+import { Box, Stack } from '@mui/material';
+import Sidebar from './Components/Sidebar';
+import Feed from './Components/Feed';
+import Add from './Components/Add';
+import Rightbar from './Components/Rightbar';
 
 function App() {
 
   const [mode, setMode] = useState("dark");
 
   return (
-    <Navbar />
+    <Box>
+    <Navbar/>
+    <Stack direction="row" spacing={2} justifyContent="space-evenly">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+    </Stack>
+    <Add/>
+  </Box>
   )
 }
 
