e1d44b9915e6d6db98c21cae0e1537542f74e408
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 02:11:40 +1200

    fix bug

--- src/theme.js
+++ src/theme.js
@@ -3,14 +3,14 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "1760a5",
+      main: "#1760a5",
       light: "skyblue",
     },
     secondary:{
         main: "#15c630",
     },
     otherColer:{
-        main: "#999",
+        main: "#999"
     }
-  },
+  }
 });
